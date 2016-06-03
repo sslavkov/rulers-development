56e5db49b2e3e65bb08951e55006a34593573873
Svetoslav Slavkov <sslavkov@cappex.com>
2016-06-03 10:35:28 -0500

    changes to ruler and title entities

--- src/main/java/com/bgrulers/repository/DynastyRepository.java
+++ src/main/java/com/bgrulers/repository/DynastyRepository.java
@@ -1,10 +1,10 @@
 package com.bgrulers.repository;
 
-import com.bgrulers.entity.Ruler;
+import com.bgrulers.entity.Dynasty;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DynastyRepository extends JpaRepository<Ruler, Long> {
+public interface DynastyRepository extends JpaRepository<Dynasty, Long> {
 	
 }
