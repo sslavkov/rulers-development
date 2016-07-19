6b5a5e69ac7b55bcd24d11f5b377b92b23041ba9
sslavkov <svet.slavkov@gmail.com>
2016-07-18 20:52:53 -0500

    adding information to list projection

--- src/main/java/com/bgrulers/entity/ruler/projection/RulerListProjection.java
+++ src/main/java/com/bgrulers/entity/ruler/projection/RulerListProjection.java
@@ -11,6 +11,7 @@ public interface RulerListProjection {
 
     Long getId();
     String getName();
+    String getInformation();
     Title getTitle();
     Date getReignStart();
     Date getReignEnd();
